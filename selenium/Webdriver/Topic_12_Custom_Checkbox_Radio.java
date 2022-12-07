7aab13fd6105c360a084fb312885355174b150e8
SunAutomation <sun.libra95@gmail.com>
2022-12-07 23:44:30 +0700

    Update Topic_12_Custom

--- selenium/Webdriver/Topic_12_Custom_Checkbox_Radio.java
+++ selenium/Webdriver/Topic_12_Custom_Checkbox_Radio.java
@@ -1,12 +1,11 @@
 package Webdriver;
 
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -14,6 +13,7 @@ import org.testng.annotations.Test;
 
 public class Topic_12_Custom_Checkbox_Radio {
 	WebDriver driver;
+	JavascriptExecutor jsExecutor;
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
 
@@ -27,14 +27,15 @@ public class Topic_12_Custom_Checkbox_Radio {
 		}
 		
 		driver = new ChromeDriver();
+		jsExecutor = (JavascriptExecutor) driver;
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		
 	}
 
-	@Test
+	//@Test
 	public void TC_01_() {
-		driver.get("https://tiemchungcovid19.gov.vn/portal/registre-person");
+		driver.get("https://tiemchungcovid19.gov.vn/portal/register-person");
 		sleepInSecond(5);
 		
 		/* CASE 1 -> FAILED */
@@ -43,9 +44,9 @@ public class Topic_12_Custom_Checkbox_Radio {
 		Assert.assertTrue(driver.findElement(By.xpath("//div[text()='Đăng ký cho người thân']/parent::div/input")).isSelected());
 	}
 
-	@Test
+	//@Test
 	public void TC_02_() {
-		driver.get("https://tiemchungcovid19.gov.vn/portal/registre-person");
+		driver.get("https://tiemchungcovid19.gov.vn/portal/register-person");
 		sleepInSecond(5);
 		
 		/* CASE 2 -> FAILED */
@@ -54,20 +55,31 @@ public class Topic_12_Custom_Checkbox_Radio {
 		Assert.assertTrue(driver.findElement(By.xpath("//div[text()='Đăng ký cho người thân']/parent::label")).isSelected());
 	}
 	
-	@Test
+	//@Test
 	public void TC_03_() {
-		driver.get("https://tiemchungcovid19.gov.vn/portal/registre-person");
+		driver.get("https://tiemchungcovid19.gov.vn/portal/register-person");
 		sleepInSecond(5);
 		
 		/* CASE 3 -> PASSED nhưng rườm rà */
 		//Dùng thẻ khác input để click (span/div/label/...). Nhưng dùng input để verify vì hàm isSelected chỉ work với thẻ input
 		driver.findElement(By.xpath("//div[text()='Đăng ký cho người thân']/parent::label")).click();
-		Assert.assertTrue(driver.findElement(By.xpath("//div[text()='Đăng ký cho người thân']/parent::label//div[text()='Đăng ký cho người thân']/parent::div/input")).isSelected());
+		Assert.assertTrue(driver.findElement(By.xpath("//div[text()='Đăng ký cho người thân']/preceding-sibling::div/input")).isSelected());
 	}
 
 	@Test
 	public void TC_04_() {
-
+		/* CASE 4 -> PASSED (kết hợp với thư viện của Javascript - JavascriptExecutor) */
+		//Thẻ input bị ẩn nhưng vẫn muốn dùng để click (Hàm click của WebElement sẽ ko thao tác vào element bị ẩn được)
+		//=> Dùng hàm click của Javascript để click vào element bị ẩn
+		//Selenium cung cấp 1 thư viện để nhúng các đoạn code JS vào kịch bản test => JavascriptExecutor
+		driver.get("https://tiemchungcovid19.gov.vn/portal/register-person");
+		sleepInSecond(3);
+		
+		By radioButton = By.xpath("//div[text()='Đăng ký cho người thân']/preceding-sibling::div/input");
+		jsExecutor.executeScript("arguments[0].click();", driver.findElement(radioButton));
+		sleepInSecond(3);
+		Assert.assertTrue(driver.findElement(radioButton).isSelected());
+		
 	}
 	
 	public void sleepInSecond(long timeInSecond) {
